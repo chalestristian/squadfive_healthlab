655d362e1c65a9883b31d06fb6df4e450adbdb23
Thales <thales.cristianeugenio@gmail.com>
2021-05-28 15:35:40 -0300

    post implementado - acucar

---
+++ solucao_web/src/app/pages/aasubpages-content/alimentar/alimentar-content/menosacucar/menosacucar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menosacucar',
+  templateUrl: './menosacucar.component.html',
+  styleUrls: ['./menosacucar.component.scss']
+})
+export class MenosacucarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
