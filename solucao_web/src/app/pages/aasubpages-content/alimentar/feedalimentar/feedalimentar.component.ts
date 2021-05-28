53f69e122048c57d9f1155d4bf5ec4f17ba880b8
Thales <thales.cristianeugenio@gmail.com>
2021-05-28 12:36:22 -0300

    criando os feeds

---
+++ solucao_web/src/app/pages/aasubpages-content/alimentar/feedalimentar/feedalimentar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-feedalimentar',
+  templateUrl: './feedalimentar.component.html',
+  styleUrls: ['./feedalimentar.component.scss']
+})
+export class FeedalimentarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
